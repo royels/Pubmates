d0210b62a3e244ed509388eba183363d06544bf0
royels <docmentz@gmail.com>
2016-03-04 21:00:00 +0100

    COmmit from Docean

--- lib/pubmates.ex
+++ lib/pubmates.ex
@@ -2,8 +2,21 @@ defmodule Pubmates do
   use Application
   use HTTPoison.Base
 
-  @expected_fields ~w(
     
+  @api_base "https://api.postmates.com/"
+
+
+  @expected_fields ~w(
+    fee currency dropoff_eta duration expires
+
+    properties features
+
+    status complete pickup_eta 
+    dropoff_deadline quote_id customer_signature_img_href
+    manifest dropoff_identifier courier related_deliveries
+
+    
+  )
 
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
@@ -21,7 +34,28 @@ defmodule Pubmates do
     Supervisor.start_link(children, opts)
   end
   def process_url(endpoint) do
+    endpoint
+  end
+
+
+  def process_response_body(body) do
+    body
+    |> Poison.decode!
+    |> Dict.take(@expected_fields)
+    |> Enum.map(fn({k, v}) -> {String.to_atom(k), v} end)
+  end
+
+  def base_api_host do
+    @api_base
+  end
+
+  def customer_prefix do
+    "v1/customers/"
+  end
+
+  def key do
+    System.get_env("POSTMATES_API_KEY")
+  end
     
 
-  end
 end
